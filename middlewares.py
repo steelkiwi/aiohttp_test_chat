0e2acb24398ad4cbf354c28dd0a5f458169d00a5
Crandel <cradlemann@gmail.com>
2016-02-27 12:18:10 +0200

    change mongo credentials

--- middlewares.py
+++ middlewares.py
@@ -10,9 +10,9 @@ async def db_handler(app, handler):
             response = await handler(request)
             return response
 
-        client = ma.AsyncIOMotorClient(MONGO_HOST, MONGO_PORT)
+        client = ma.AsyncIOMotorClient(MONGO_HOST)
         db = client[MONGO_DB_NAME]
-        db.authenticate(MONGO_USER, MONGO_PASS)
+        print(db, 'db end')
         request.db = db
         response = await handler(request)
         client.close()
--- settings.py
+++ settings.py
@@ -11,10 +11,7 @@ SITE_HOST = env.str('HOST')
 SITE_PORT = env.int('PORT')
 SECRET_KEY = env.str('SECRET_KEY')
 MONGO_HOST = env.str('MONGO_HOST')
-MONGO_PORT = env.int('MONGO_PORT')
 MONGO_DB_NAME = env.str('MONGO_DB_NAME')
-MONGO_USER = env.str('MONGO_USER')
-MONGO_PASS = env.str('MONGO_PASS')
 
 MESSAGE_COLLECTION = 'messages'
 USER_COLLECTION = 'users'
