8e888a907fa631266fac97512e3179ea0b35289b
Crandel <cradlemann@gmail.com>
2016-02-27 09:36:23 +0200

    add Procfile and change settings for Heroku

--- settings.py
+++ settings.py
@@ -1,11 +1,14 @@
+from os.path import isfile
 from envparse import env
 
-env.read_envfile('.env')
+
+if isfile('.env'):
+    env.read_envfile('.env')
 
 DEBUG = env.bool('DEBUG', default=False)
 
-SITE_HOST = env.str('SITE_HOST')
-SITE_PORT = env.int('SITE_PORT')
+SITE_HOST = env.str('HOST')
+SITE_PORT = env.int('PORT')
 SECRET_KEY = env.str('SECRET_KEY')
 MONGO_HOST = env.str('MONGO_HOST')
 MONGO_PORT = env.int('MONGO_PORT')
